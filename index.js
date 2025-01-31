71ab57777b8616d2776678eaf0923424f4ad4235
Abhishek Mangal <abhishek.mangal@indiaresults.com>
2025-01-31 12:01:42 +0530

    changed port 7080

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 7080;
 
 app.get("/", (req, res) => {
     res.send("Hello, Jenkins! Your Node.js app is running on IIS!");
